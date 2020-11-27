65a17e02082611accf6b5cbd76cf19a18e4f6f86
ken <c.uvsuv@163.com>
2020-11-27 11:35:22 +0800

    update flutter web

--- lib/data/Source.dart
+++ lib/data/Source.dart
@@ -28,6 +28,16 @@ class Source{
           "https://desktopwalls.net/animals/hamster-listening-to-music-headphones-desktop-wallpaper","Hamster Listening To Music"));
       _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2014/07/Cat%20Staring%20Big%20Eyes%20And%20Ears%20Desktop%20Wallpaper.jpg",
           "https://desktopwalls.net/animals/cat-staring-big-eyes-and-ears-desktop-wallpaper","Cat Staring Big Eyes And Ears"));
+
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2015/12/Sparrow%20On%20Tree%20Branch%20Bokeh%20Wildlife%20Photography%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/sparrow-on-tree-branch-bokeh-wildlife-photography-desktop-wallpaper","Sparrow On Tree Branch Bokeh Wildlife"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2015/02/Cute%20Blue%20Eyed%20Kittens%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/cute-blue-eyed-kittens-desktop-wallpaper","Cute Blue Eyed Kittens"));
+
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2014/09/Chameleon%20Hunting%20Insects%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/chameleon-hunting-insects-desktop-wallpaper","Chameleon Hunting Insects"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2015/08/Brown%20Cat%20Fruits%20Close%20Up%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/brown-cat-fruits-close-up-desktop-wallpaper","Brown Cat Fruits Close Up"));
     }
     return _cardList;
   }
