6e399153d5193c61fed2444ea1944727cbcc1051
ken <c.uvsuv@163.com>
2020-11-27 10:10:56 +0800

    update flutter web

--- lib/data/Source.dart
+++ lib/data/Source.dart
@@ -13,41 +13,21 @@ class Source{
   static List _cardList = List<Data>();
   static List<Data> getCardList(){
     if(_cardList.isEmpty){
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Monte%20Carlo%20Yachts%20"
-          "Port%20Panorama%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/places/monte-carlo-yachts-port-panorama-4k-ultra-hd-desktop-wallpaper","Monte Carlo Yachts Port Panorama"));
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Seljalandsfoss%20"
-          "Waterfall%20Iceland%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/nature/seljalandsfoss-waterfall-iceland-4k-ultra-hd-desktop-wallpaper","Seljalandsfoss Waterfall Iceland"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2018/12/Cat%20Looking%20Up%20Eye%20Closeup%20Dark%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/cat-looking-up-eye-closeup-dark-desktop-wallpaper","Cat Looking Up Eye Closeup Dark"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2015/02/Leopard%20Hanging%20In%20Tree%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/leopard-hanging-in-tree-desktop-wallpaper","Leopard Hanging In Tree Desktop"));
 
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Monte%20Carlo%20Yachts%20"
-          "Port%20Panorama%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/places/monte-carlo-yachts-port-panorama-4k-ultra-hd-desktop-wallpaper","Monte Carlo Yachts Port Panorama"));
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Seljalandsfoss%20"
-          "Waterfall%20Iceland%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/nature/seljalandsfoss-waterfall-iceland-4k-ultra-hd-desktop-wallpaper","Seljalandsfoss Waterfall Iceland"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2014/06/Cute%20Baby%20Seal%20White%20Fur%20Free%20Wallpaper%20HD.jpg",
+          "https://desktopwalls.net/animals/cute-baby-seal-white-fur-free-wallpaper-hd","Cute Baby Seal White Fur"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2014/09/North%20American%20Bald%20Eagle%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/north-american-bald-eagle-desktop-wallpaper","North American Bald Eagle"));
 
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Monte%20Carlo%20Yachts%20"
-          "Port%20Panorama%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/places/monte-carlo-yachts-port-panorama-4k-ultra-hd-desktop-wallpaper","Monte Carlo Yachts Port Panorama"));
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Seljalandsfoss%20"
-          "Waterfall%20Iceland%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/nature/seljalandsfoss-waterfall-iceland-4k-ultra-hd-desktop-wallpaper","Seljalandsfoss Waterfall Iceland"));
 
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Monte%20Carlo%20Yachts%20"
-          "Port%20Panorama%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/places/monte-carlo-yachts-port-panorama-4k-ultra-hd-desktop-wallpaper","Monte Carlo Yachts Port Panorama"));
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Seljalandsfoss%20"
-          "Waterfall%20Iceland%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/nature/seljalandsfoss-waterfall-iceland-4k-ultra-hd-desktop-wallpaper","Seljalandsfoss Waterfall Iceland"));
-
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Monte%20Carlo%20Yachts%20"
-          "Port%20Panorama%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/places/monte-carlo-yachts-port-panorama-4k-ultra-hd-desktop-wallpaper","Monte Carlo Yachts Port Panorama"));
-      _cardList..add(Data("https://desktopwalls.net/wp-content/uploads/2015/09/Seljalandsfoss%20"
-          "Waterfall%20Iceland%204K%20Ultra%20HD%20Desktop%20Wallpaper.jpg",
-          "https://desktopwalls.net/nature/seljalandsfoss-waterfall-iceland-4k-ultra-hd-desktop-wallpaper","Seljalandsfoss Waterfall Iceland"));
-
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2016/03/Hamster%20Listening%20To%20Music%20Headphones%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/hamster-listening-to-music-headphones-desktop-wallpaper","Hamster Listening To Music"));
+      _cardList.add(Data("https://desktopwalls.net/wp-content/uploads/2014/07/Cat%20Staring%20Big%20Eyes%20And%20Ears%20Desktop%20Wallpaper.jpg",
+          "https://desktopwalls.net/animals/cat-staring-big-eyes-and-ears-desktop-wallpaper","Cat Staring Big Eyes And Ears"));
     }
     return _cardList;
   }
--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -69,90 +69,89 @@ class _HomeBodyState extends State<HomeBodyPage> with TickerProviderStateMixin {
     var height = Px.matchHeight(context);
     return Scaffold(
       backgroundColor: Colors.black38,
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        child: Stack(
-            children: [
-              Container(
-                height: height,
-                width: width,
-                child: Image.asset(
-                  Asset.pathByName("wallpaper_bg.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                width: width,
-                height: Px.getTitleHeight(),
-                child: Stack(
-                  children: [
-                    Hero(
-                      tag: "TitleBg",
-                      child: ClipRect(
-                        child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
-                            child: Container(color: Colors.white.withOpacity(0.2))),
-                      ),
-                    ),
-                    FadeTransition(
-                      opacity: _alphaAnimation,
-                      child: Container(
-                        width: width,
-                        height: Px.getTitleHeight(),
-                        child: Center(
-                          child: Text(
-                            "4K HD WallPaper",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.tealAccent,
-                                fontSize: Px.getTitleSize(),
-                                fontFamily: "SansitaSwashed"),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned.fill(
-                top: Px.getCardPaddingTop(),
-                left: 0,
-                right: 0,
-                child: SlideTransition(
-                  position: _bottomAnimation,
+      body: Stack(
+        children: [
+          Container(
+            height: height,
+            width: width,
+            child: Image.asset(
+              Asset.pathByName("wallpaper_bg.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: width,
+            height: Px.getTitleHeight(),
+            child: Stack(
+              children: [
+                Hero(
+                  tag: "TitleBg",
                   child: ClipRect(
                     child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
-                      child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white54,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(Px.getCardRadius()),
-                                  topRight: Radius.circular(Px.getCardRadius()))),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GridView.builder(
-                              shrinkWrap: true,
-                              controller: _scrollController,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (context, position) {
-                                return _buildView(context, position);
-                              },
-                              itemCount: Source.getCardList().length,
-                              gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
-                                  childAspectRatio: 1.0,
-                                  maxCrossAxisExtent: Px.getCardExtent(),
-                              ),
-                            ),
-                          )),
+                        filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
+                        child: Container(color: Colors.white.withOpacity(0.2))),
+                  ),
+                ),
+                FadeTransition(
+                  opacity: _alphaAnimation,
+                  child: Container(
+                    width: width,
+                    height: Px.getTitleHeight(),
+                    child: Center(
+                      child: Text(
+                        "4K HD WallPaper",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.tealAccent,
+                            fontSize: Px.getTitleSize(),
+                            fontFamily: "SansitaSwashed"),
+                      ),
                     ),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
+
+          SingleChildScrollView(
+            controller: _scrollController,
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: EdgeInsets.only(top: Px.getCardPaddingTop()),
+              child: SlideTransition(
+                position: _bottomAnimation,
+                child: ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white54,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(Px.getCardRadius()),
+                                topRight: Radius.circular(Px.getCardRadius()))),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: GridView.builder(
+                            shrinkWrap: true,
+                            controller: _scrollController,
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (context, position) {
+                              return _buildView(context, position);
+                            },
+                            itemCount: Source.getCardList().length,
+                            gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
+                              childAspectRatio: 1.0,
+                              maxCrossAxisExtent: Px.getCardExtent(),
+                            ),
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
